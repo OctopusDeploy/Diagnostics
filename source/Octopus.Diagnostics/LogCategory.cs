c2dc2b844f6a7bb80f8671c67b1a49ed02b03d37
Shannon Lewis <slewis74@gmail.com>
2016-10-18 16:36:44 +1000

    Initial commit

---
+++ source/Octopus.Diagnostics/LogCategory.cs
@@ -0,0 +1,16 @@
+﻿namespace Octopus.Diagnostics
+{
+    public enum LogCategory
+    {
+        Trace = 1,
+        Verbose = 100,
+        Info = 200,
+        Planned = 201,
+        Abandoned = 220,
+        Progress = 230,
+        Finished = 240,
+        Warning = 300,
+        Error = 400,
+        Fatal = 500
+    }
+}
