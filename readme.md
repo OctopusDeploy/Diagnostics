121a270b70db3dc4051a5138b330253f445727aa
Shannon Lewis <slewis74@gmail.com>
2016-10-18 17:08:52 +1000

    Build setup

---
+++ readme.md
@@ -0,0 +1,3 @@
+This repository contains the core Diagnostics contracts for [Octopus Deploy][1] server.
+
+[1]: https://octopus.com
---
+++ source/Octopus.Diagnostics/Properties/AssemblyInfo.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyDescription("Diagnostics interfaces for Octopus, an opinionated deployment solution for .NET applications")]
+[assembly: AssemblyCompany("Octopus Deploy")]
+[assembly: AssemblyProduct("Octopus, an opinionated deployment solution for .NET applications")]
+[assembly: AssemblyCopyright("Copyright © Octopus Deploy 2016")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("Octopus.Diagnostics")]
