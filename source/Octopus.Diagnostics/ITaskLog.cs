efca6b90c6a048750abc1b03d1cb0ea5da50ea55
slewis74 <slewis74@gmail.com>
2021-01-07 17:01:38 +1000

    Added ITaskLog, as Server and Calamari test code both use this. We might this to a new library at some point.

---
+++ source/Octopus.Diagnostics/ITaskLog.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Octopus.Diagnostics
+{
+    public interface ITaskLog : ILog, IDisposable
+    {
+        ITaskLogContext Context { get; }
+
+        bool IsVerboseEnabled { get; }
+        bool IsErrorEnabled { get; }
+        bool IsFatalEnabled { get; }
+        bool IsInfoEnabled { get; }
+        bool IsTraceEnabled { get; }
+        bool IsWarnEnabled { get; }
+
+        /// <summary>
+        /// Opens a new child block for logging.
+        /// </summary>
+        /// <param name="messageText">Title of the new block.</param>
+        /// <returns>A child <see cref="ITaskLog" />.</returns>
+        ITaskLog OpenBlock(string messageText);
+
+        /// <summary>
+        /// Opens a new child block for logging.
+        /// </summary>
+        /// <param name="messageFormat">Format string for the message.</param>
+        /// <param name="args">Arguments for the format string.</param>
+        /// <returns>A child <see cref="ITaskLog" />.</returns>
+        [StringFormatMethod("messageFormat")]
+        ITaskLog OpenBlock(string messageFormat, params object[] args);
+
+        /// <summary>
+        /// Plans a new block of output that will be used in the future for grouping child blocks for logging.
+        /// </summary>
+        /// <param name="messageText">Title of the new block.</param>
+        /// <returns>An <see cref="ITaskLogContext" /> that will automatically revert the current block when disposed.</returns>
+        ITaskLogContext PlanGroupedBlock(string messageText);
+
+        /// <summary>
+        /// Plans a new block of log output that will be used in the future. This is typically used for high-level log
+        /// information, such as the steps in a big deployment process.
+        /// </summary>
+        /// <param name="messageText">Title of the new block.</param>
+        /// <returns>An <see cref="IDisposable" /> that will automatically revert the current block when disposed.</returns>
+        ITaskLogContext PlanFutureBlock(string messageText);
+
+        /// <summary>
+        /// Plans a new block of log output that will be used in the future. This is typically used for high-level log
+        /// information, such as the steps in a big deployment process.
+        /// </summary>
+        /// <param name="messageFormat">Format string for the message.</param>
+        /// <param name="args">Arguments for the format string.</param>
+        /// <returns>An <see cref="IDisposable" /> that will automatically revert the current block when disposed.</returns>
+        [StringFormatMethod("messageFormat")]
+        ITaskLogContext PlanFutureBlock(string messageFormat, params object[] args);
+
+        /// <summary>
+        /// Switches to a new logging context on the current thread, allowing you to begin logging within a block previously
+        /// begun using <see cref="o:OpenBlock" /> or <see cref="o:PlanFutureBlock" />.
+        /// </summary>
+        /// <param name="logContext">The <see cref="ITaskLogContext" /> to switch to.</param>
+        /// <returns>A child <see cref="ITaskLog" />.</returns>
+        ITaskLog WithinBlock(ITaskLogContext logContext);
+
+        /// <summary>
+        /// Marks the current block as abandoned. Abandoned blocks won't be shown in the task log.
+        /// </summary>
+        void Abandon();
+
+        /// <summary>
+        /// If a block was previously abandoned using <see cref="Abandon" />, calling <see cref="Reinstate" /> will un-abandon
+        /// it.
+        /// </summary>
+        void Reinstate();
+
+        /// <summary>
+        /// Marks the current block as finished. If there were any errors, it will be finished with errors. If there were no
+        /// errors, it will be assumed to be successful.
+        /// </summary>
+        void Finish();
+
+        bool IsEnabled(LogCategory category);
+
+        void UpdateProgress(int progressPercentage, string messageText);
+
+        [StringFormatMethod("messageFormat")]
+        void UpdateProgressFormat(int progressPercentage, string messageFormat, params object[] args);
+    }
+}
---
+++ source/Octopus.Diagnostics/ITaskLogContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Octopus.Diagnostics
+{
+    public interface ITaskLogContext
+    {
+        string CorrelationId { get; }
+
+        ITaskLogContext CreateChild(string[]? sensitiveValues = null);
+
+        /// <summary>
+        /// Adds additional sensitive-variables to the LogContext.
+        /// </summary>
+        /// <returns>The existing LogContext</returns>
+        ITaskLogContext WithSensitiveValues(string[] sensitiveValues);
+
+        /// <summary>
+        /// Adds an additional sensitive-variable to the LogContext.
+        /// </summary>
+        /// <returns>The existing LogContext</returns>
+        ITaskLogContext WithSensitiveValue(string sensitiveValue);
+
+        void Flush();
+    }
+}
